4f27b109503697b38da59704c3413b6bc152e7b9
Onur Onat Surmeli <onatsurmeli@gmail.com>
2023-03-27 01:42:37 +0200

    added a word list to pick the winning word randomly

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import random
+
+word_tuple = ("python", "java", "swift", "javascript")
 print("""
 H A N G M A N
 """)
-if input("Guess the word:") == "python":
+if input("Guess the word:") == random.choice(word_tuple):
     print("You survived!")
 else:
     print("You lost!")
