c46dafe323af0a67b0eb6e6fd8593e96a479f27a
Onur Onat Surmeli <onatsurmeli@gmail.com>
2023-03-27 02:10:50 +0200

    added a tip that shows the first 3 letters of the winning word, as well as the total length

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ word_tuple = ("python", "java", "swift", "javascript")
 print("""
 H A N G M A N
 """)
-if input("Guess the word:") == random.choice(word_tuple):
+chosen_word = random.choice(word_tuple)
+if input("Guess the word " + chosen_word[:3] + "-" * (len(chosen_word) - 3) + ":") == chosen_word:
     print("You survived!")
 else:
     print("You lost!")
